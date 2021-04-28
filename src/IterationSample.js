113c5c93961d39df5ebdc95805e8ef0c78e4a6b8
Bennyziio <bapjoo2@gmail.com>
2021-04-28 23:20:43 +0900

    20210428 컴포넌트 반복 및 데이터 추가, 제거 기능 추가

---
+++ src/IterationSample.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const IterationSample = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: '눈사람' },
+    { id: 2, text: '얼음' },
+    { id: 3, text: '눈' },
+    { id: 4, text: '바람' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+  const onChange = (e) => setInputText(e.target.value);
+
+  // 클릭시 데이터 추가 기능 추가
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, // nextId 값을 id로 설정하고
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId 값에 1을 더해준다.
+    setNames(nextNames); //  names 값을 업데이트 한다.
+    setInputText(''); // inputText를 비운다
+  };
+  // 더블클릭시 데이터 제거 기능 추가
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+  /*
+  const names = ['눈사람', '얼음', '눈', '바람'];
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  return (
+      */
+  /*
+    <ul>
+      <li>눈사람</li>
+      <li>얼음</li>
+      <li>눈</li>
+      <li>바람</li>
+    </ul>
+    */
+  //<ul>{nameList}</ul>
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
+};
+
+export default IterationSample;
